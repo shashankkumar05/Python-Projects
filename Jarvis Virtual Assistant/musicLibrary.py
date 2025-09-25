7a0c40b2163d1765a5ec4ece58e5601dbf1b2320
Shashank Pandey <kumarshashank3185@gmail.com>
2025-09-25 11:08:59 +0530

    Add files via upload

---
+++ Jarvis Virtual Assistant/musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "sahiba": "https://youtu.be/n2dVFdqMYGA?list=RDWpBn9w-Js_c",
+    "sundari": "https://youtu.be/WpBn9w-Js_c?list=RDWpBn9w-Js_c",
+    "guru": "https://youtu.be/c-FKlE3_kHo?list=RDWpBn9w-Js_c",
+    "sai": "https://youtu.be/sUuLY8-LjKM?list=RDWpBn9w-Js_c"
+}
